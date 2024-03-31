106ac9b6a894cce994dd15822db42da26b696e11
anamariagds <gomesabam@gmail.com>
2024-03-31 09:35:51 -0300

    Criação models Animal, HistoricoSaude e django admin

--- animal/admin.py
+++ animal/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from animal.models import HitoricoSaude, Animal
 
 # Register your models here.
+@admin.register(HitoricoSaude)
+class HistoricoSaudeAdmin(admin.ModelAdmin):
+    list_display = ['castrado', 'vacinas', 'idas_vet']
+
+@admin.register(Animal)
+class AnimalAdmin(admin.ModelAdmin):
+    list_display = ['nome', 'especie', 'idade', 'raca', 'data_cadastro', 'historico_saude']
+    search_fields = ['especie','idade', 'raca']
+    list_filter = ['data_cadastro']
--- animal/models.py
+++ animal/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class HitoricoSaude(models.Model):
+    choices_castracao = (('C', 'Castrado'),
+                        ('N', 'Não castrado'))
+    castrado = models.CharField(max_length=1, choices=choices_castracao, default='N')
+    vacinas = models.CharField(max_length=200)
+    idas_vet = models.TextField()
+
+    def __str__(self):
+        return f'{self.castrado}'
+    
+
+class Animal(models.Model):
+    nome = models.CharField(max_length=100)
+    especie = models.CharField(max_length=50)
+    idade = models.IntegerField()
+    raca = models.CharField(max_length=100)
+    data_cadastro = models.DateTimeField(auto_now_add=True)
+    historico_saude = models.ForeignKey(HitoricoSaude, on_delete=models.CASCADE)
