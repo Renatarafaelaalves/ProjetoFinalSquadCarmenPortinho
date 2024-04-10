42b51ae3745b815754da31dcb109487810190681
anamariagds <gomesabam@gmail.com>
2024-04-10 17:01:45 -0300

    Filtro de busca implementado na view da pagina inicial

--- animal/urls.py
+++ animal/urls.py
@@ -4,6 +4,6 @@ from animal.views import listar_animais, ver_animal, pagina_inicial
 app_name = 'animais'
 urlpatterns = [
     path('', pagina_inicial, name='pagina_inicial'),
-    path('listar/', listar_animais, name='listar_animais'),
+    path('listar_animais/', listar_animais, name='listar_animais'),
     path('<int:id_animal>/', ver_animal, name='ver_animal')
 ]
